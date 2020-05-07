978a76a10f0f494edecee44a1930d1723d15f2e3
abdulmuksith3 <57699415+abdulmuksith3@users.noreply.github.com>
2020-05-08 00:31:25 +0300

    Login

--- mainpages/Authentication.js
+++ mainpages/Authentication.js
@@ -5,13 +5,20 @@ import {
   View,
   TextInput,
   TouchableOpacity,
+  Dimensions,
 } from "react-native";
 import firebase from "firebase/app";
 import "firebase/auth";
 import db from "../db";
+import LottieView from "lottie-react-native";
+import Icon from "react-native-vector-icons/MaterialCommunityIcons";
+import { Input } from "react-native-elements";
+
+import { ButtonGroup } from "react-native-elements";
 
 export default function Authentication(props) {
-  const [view, setView] = useState("Register");
+  const [view, setView] = useState(0);
+  const buttons = ["Login", "Register"];
 
   // ***** Register useState *****
 
@@ -89,8 +96,6 @@ export default function Authentication(props) {
           console.log(err);
         });
 
-      
-
       // calling createUserInfo and waiting for it before moving the user to login page
       await createUserInfo();
     } catch (error) {
@@ -159,7 +164,7 @@ export default function Authentication(props) {
           point: null,
         },
         // checking if the user used referral code and giving him a token if he did
-        tokens: referralStatus === true ? 1 : 0,
+        tokens: 0,
         location: null,
         privacy: {
           emailP: false,
@@ -194,7 +199,7 @@ export default function Authentication(props) {
       .auth()
       .signInWithEmailAndPassword(loginEmail, loginPassword)
       .then(() => {
-        console.log("successful login!")
+        console.log("successful login!");
       })
       .catch((err) => {
         alert(err.message);
@@ -211,98 +216,169 @@ export default function Authentication(props) {
       .catch((err) => alert(err.message));
   };
 
-  return view === "Register" ? (
+  return (
     <View style={styles.container}>
-      <View>
-        <TextInput
-          onChangeText={setRegisterEmail}
-          placeholder="username@email.com"
-          value={registerEmail}
+      <View style={styles.header}>
+        <LottieView
+          source={require("../assets/login.json")}
+          autoPlay
+          loop
+          style={{ position: "relative", width: "50%" }}
         />
       </View>
-      <View>
-        <TextInput
-          onChangeText={setRegisterPassword}
-          placeholder="Password"
-          secureTextEntry={true}
-          value={registerPassword}
+      <View style={{ alignItems: "center" }}>
+        <ButtonGroup
+          onPress={() => (view === 1 ? setView(0) : setView(1))}
+          selectedIndex={view}
+          buttons={buttons}
+          containerStyle={{
+            // height: 100,
+            backgroundColor: "transparent",
+            borderColor: "gray",
+            borderBottomColor: "gray",
+            marginTop: 50,
+            width: "80%",
+          }}
+          selectedButtonStyle={{
+            backgroundColor: "#06575c",
+            borderBottomColor: "black",
+          }}
+          innerBorderStyle={{
+            color: "gray",
+          }}
         />
-      </View>
-      <View>
-        <TextInput
-          onChangeText={setConfirmRegisterPassword}
-          placeholder="Confirm Password"
-          secureTextEntry={true}
-          value={confirmRegisterPassword}
-        />
-      </View>
-      <View>
-        <TextInput
-          onChangeText={setDisplayName}
-          placeholder="Display Name"
-          value={displayName}
-        />
-      </View>
-      <View>
-        <TextInput
-          onChangeText={setPhone}
-          keyboardType="number-pad"
-          placeholder="Phone No."
-          value={phone}
-        />
-      </View>
-      <TouchableOpacity onPress={handleRegister}>
-        <Text>Sign Up!</Text>
-      </TouchableOpacity>
-
-      <TouchableOpacity onPress={() => setView("Login")}>
-        <Text>Already Have Account?</Text>
-      </TouchableOpacity>
-    </View>
-  ) : view === "Login" ? (
-    <View style={styles.container}>
-      <View>
-        <TextInput
-          onChangeText={setLoginEmail}
-          placeholder="username@email.com"
-          value={loginEmail}
-        />
-      </View>
-      <View>
-        <TextInput
-          onChangeText={setLoginPassword}
-          placeholder="Password"
-          secureTextEntry={true}
-          value={loginPassword}
-        />
-        <TouchableOpacity onPress={() => setView("ForgotPass")}>
-          <Text>Forgot Password?</Text>
-        </TouchableOpacity>
       </View>
 
-      <TouchableOpacity onPress={() => handleLogin()}>
-        <Text>Login</Text>
-      </TouchableOpacity>
+      {view === 1 ? (
+        <View style={styles.containerRegister}>
+          <View>
+            <TextInput
+              onChangeText={setRegisterEmail}
+              placeholder="username@email.com"
+              value={registerEmail}
+            />
+          </View>
+          <View>
+            <TextInput
+              onChangeText={setRegisterPassword}
+              placeholder="Password"
+              secureTextEntry={true}
+              value={registerPassword}
+            />
+          </View>
+          <View>
+            <TextInput
+              onChangeText={setConfirmRegisterPassword}
+              placeholder="Confirm Password"
+              secureTextEntry={true}
+              value={confirmRegisterPassword}
+            />
+          </View>
+          <View>
+            <TextInput
+              onChangeText={setDisplayName}
+              placeholder="Display Name"
+              value={displayName}
+            />
+          </View>
+          <View>
+            <TextInput
+              onChangeText={setPhone}
+              keyboardType="number-pad"
+              placeholder="Phone No."
+              value={phone}
+            />
+          </View>
+          <TouchableOpacity onPress={handleRegister}>
+            <Text>Sign Up!</Text>
+          </TouchableOpacity>
 
-      <TouchableOpacity onPress={() => setView("Register")}>
-        <Text>Create New Account</Text>
-      </TouchableOpacity>
-    </View>
-  ) : (
-    <View>
-      <Text>Forgot Password</Text>
-      <TextInput
-        placeholder="Email"
-        value={forgotPassEmail}
-        onChangeText={setForgotPassEmail}
-      />
-      <TouchableOpacity onPress={handleSubmit}>
-        <Text>Submit</Text>
-      </TouchableOpacity>
+          <TouchableOpacity onPress={() => setView("Login")}>
+            <Text>Already Have Account?</Text>
+          </TouchableOpacity>
+        </View>
+      ) : view === 0 ? (
+        <View style={styles.containerLogin}>
+          {/* <View style={styles.header}>
+            <Text style={styles.headerText}>Sign in</Text>
+          </View> */}
+          <View style={styles.form}>
+            {/* <LottieView
+              source={require("../assets/login.json")}
+              autoPlay
+              loop
+              style={{ position: "relative", width: "50%" }}
+            /> */}
+            <View style={{ flex: 6, width: "100%" }}>
+              <Input
+                inputContainerStyle={{ borderBottomWidth: 0 }}
+                leftIcon={
+                  <Icon name="email-outline" size={20} color="lightgray" />
+                }
+                containerStyle={styles.Inputs}
+                onChangeText={setLoginEmail}
+                placeholder="E-mail"
+                value={loginEmail}
+                // errorMessage="Error"
+                // errorStyle={{ color: "blue" }}
+                // renderErrorMessage
+              />
+              <Input
+                inputContainerStyle={{ borderBottomWidth: 0 }}
+                leftIcon={<Icon name="key" size={20} color="lightgray" />}
+                containerStyle={styles.Inputs}
+                onChangeText={setLoginPassword}
+                placeholder="Password"
+                secureTextEntry={true}
+                value={loginPassword}
+              />
+            </View>
 
-      <TouchableOpacity onPress={() => setView("Login")}>
-        <Text>Back to Login</Text>
-      </TouchableOpacity>
+            <View
+              style={{
+                // flexDirection: "row",
+                // justifyContent: "center",
+                flex: 1,
+                // backgroundColor: "green",
+                width: "100%",
+              }}
+            >
+              {/* <TouchableOpacity style={styles.Buttons}>
+                <Text>Access Code</Text>
+              </TouchableOpacity> */}
+              <TouchableOpacity
+                style={styles.loginButton}
+                onPress={() => handleLogin()}
+              >
+                <Text style={{ color: "white" }}>Login</Text>
+              </TouchableOpacity>
+            </View>
+          </View>
+
+          {/* <View style={styles.footer}>
+            <Text style={{ color: "white" }}>Don't Have an Account? </Text>
+            <TouchableOpacity>
+              <Text style={{ color: "darkblue" }}>Register</Text>
+            </TouchableOpacity>
+          </View> */}
+        </View>
+      ) : (
+        <View>
+          <Text>Forgot Password</Text>
+          <TextInput
+            placeholder="Email"
+            value={forgotPassEmail}
+            onChangeText={setForgotPassEmail}
+          />
+          <TouchableOpacity onPress={handleSubmit}>
+            <Text>Submit</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity onPress={() => setView("Login")}>
+            <Text>Back to Login</Text>
+          </TouchableOpacity>
+        </View>
+      )}
     </View>
   );
 }
@@ -310,10 +386,102 @@ export default function Authentication(props) {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
+    backgroundColor: "#0A5155",
     alignItems: "center",
+    //justifyContent: "center",
+    // flexDirection: "column",
+    // paddingHorizontal: 20,
+    width: Math.round(Dimensions.get("window").width),
+    height: Math.round(Dimensions.get("window").height),
+  },
+  containerLogin: {
+    flex: 1,
+    backgroundColor: "#06575c",
+    width: "80%",
+    marginTop: -5,
+    marginBottom: "5%",
+    borderWidth: 1,
+    borderTopWidth: 0,
+    borderColor: "gray",
+    // alignItems: "center",
+    // justifyContent: "flex-start",
+    // flexDirection: "column",
+    // paddingHorizontal: 20,
+    //width: Math.round(Dimensions.get("window").width),
+    //height: Math.round(Dimensions.get("window").height),
+  },
+  containerRegister: {
+    flex: 1,
+    backgroundColor: "#0A5155",
+    //alignItems: "center",
+    //justifyContent: "center",
+    // flexDirection: "column",
+    // paddingHorizontal: 20,
+    //width: Math.round(Dimensions.get("window").width),
+    //height: Math.round(Dimensions.get("window").height),
+  },
+  headerText: {
+    alignItems: "center",
+    textAlign: "center",
+    color: "white",
+    fontSize: 34,
+    fontWeight: "bold",
+  },
+  footer: {
+    textAlign: "center",
     justifyContent: "center",
-    flexDirection: "column",
-    paddingHorizontal: 20,
+    alignItems: "center",
+    backgroundColor: "#0A5155",
+    flex: 1,
+    flexDirection: "row",
+  },
+  form: {
+    flex: 4,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  Inputs: {
+    borderRadius: 8,
+    borderWidth: 1,
+    borderColor: "white",
+    height: 50,
+    width: "80%",
+    alignSelf: "center",
+    opacity: 0.8,
+    paddingLeft: 12,
+    marginTop: 20,
+  },
+  Buttons: {
+    borderRadius: 8,
+    borderWidth: 1,
+    backgroundColor: "white",
+    height: 35,
+    width: "30%",
+    alignSelf: "center",
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 20,
+    // marginRight:8,
+    marginStart: "2%",
+    marginEnd: "2%",
+  },
+  loginButton: {
+    backgroundColor: "#3e875b",
+    height: "100%",
+    width: "100%",
+    alignSelf: "center",
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 13,
+    // marginRight:8,
+    marginStart: "2%",
+    marginEnd: "2%",
+  },
+  header: {
+    justifyContent: "center",
+    alignItems: "center",
+    // backgroundColor: "red",
+    flex: 1,
+    // paddingTop: 20,
   },
 });
