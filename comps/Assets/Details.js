d343609bbc3a82382701b68deb629670fd4bdb07
amalhajo <amalhajo@hotmail.com>
2020-05-12 22:16:47 +0300

    fixes

--- comps/Assets/Details.js
+++ comps/Assets/Details.js
@@ -20,8 +20,6 @@ import "firebase/auth";
 import db from "../../db.js";
 require("firebase/firestore");
 
-
-
 export default function Details(props) {
   const tName=props.navigation.getParam("tName",'failed')
   const sName=props.navigation.getParam("sName",'failed')
@@ -59,9 +57,6 @@ export default function Details(props) {
     
 //   } 
 
-
-
-
   return (
     <View style={styles.container}>
       {console.log('asset return',asset)}
@@ -95,40 +90,6 @@ Details.navigationOptions = (props) => ({
     headerTintStyle: { fontWeight: "bold" }
 })
 
-function DevelopmentModeNotice() {
-  if (__DEV__) {
-    const learnMoreButton = (
-      <Text onPress={handleLearnMorePress} style={styles.helpLinkText}>
-        Learn more
-      </Text>
-    );
-
-    return (
-      <Text style={styles.developmentModeText}>
-        Development mode is enabled: your app will be slower but you can use
-        useful development tools. {learnMoreButton}
-      </Text>
-    );
-  } else {
-    return (
-      <Text style={styles.developmentModeText}>
-        You are not in development mode: your app will run at full speed.
-      </Text>
-    );
-  }
-}
-
-function handleLearnMorePress() {
-  WebBrowser.openBrowserAsync(
-    "https://docs.expo.io/versions/latest/workflow/development-mode/"
-  );
-}
-
-function handleHelpPress() {
-  WebBrowser.openBrowserAsync(
-    "https://docs.expo.io/versions/latest/workflow/up-and-running/#cant-see-your-changes"
-  );
-}
 
 const styles = StyleSheet.create({
   container: {
--- navigation/HomeStack.js
+++ navigation/HomeStack.js
@@ -3,11 +3,22 @@ import HomePage from "../comps/HomePage";
 import Payment from "../comps/Assets/Payment"
 import CheckOut from "../comps/Assets/CheckOut"
 import Assets from "../comps/Assets/Assets"
+import Details from "../comps/Assets/Details"
+import Sections from "../comps/Assets/Sections"
+import List from "../comps/Assets/List"
+import Types from "../comps/Assets/Types"
+
 const HomeStack = createStackNavigator({
+  
+  Types: Types,
   Assets: Assets,
   Home: HomePage,
- 
-  
+  Sections: Sections,
+  List: List,
+  Details: Details,
+  Payment: Payment,
+  CheckOut: CheckOut
+
 });
 
 export default HomeStack;
