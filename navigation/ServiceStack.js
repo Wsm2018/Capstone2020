5d9fac27c418bf3d98a7a5ed0a50a9460c2e832f
iMultiBar <shbshbal@gmail.com>
2020-05-17 15:46:59 +0300

    deleted becose is taken by other member

--- navigation/ServiceStack.js
+++
@@ -1,8 +0,0 @@
-import { createStackNavigator } from "react-navigation-stack";
-import ServicePage from "../comps/ServicePage";
-
-const ServiceStack = createStackNavigator({
-  Service: ServicePage,
-});
-
-export default ServiceStack;
