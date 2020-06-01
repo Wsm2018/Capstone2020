10a800c3d48db818f5c5fd7af519ca0dea396dc0
abubaker70 <57699491+abubaker70@users.noreply.github.com>
2020-06-01 14:41:20 +0300

    Update App.js

--- App.js
+++ App.js
@@ -11,8 +11,6 @@ import {
   AsyncStorage,
 } from "react-native";
 import Authentication from "./mainpages/Authentication";
-import Assets from "./comps/Assets/Assets";
-
 console.disableYellowBox = true;
 import firebase from "firebase/app";
 import "firebase/auth";
@@ -28,40 +26,85 @@ if (!global.atob) {
 
 import { createAppContainer } from "react-navigation";
 import { createBottomTabNavigator } from "react-navigation-tabs";
+import { createDrawerNavigator, DrawerItems } from "react-navigation-drawer";
 import HomeStack from "./navigation/HomeStack";
 import ProfileStack from "./navigation/ProfileStack";
 import FriendsStack from "./comps/Friends/FriendsScreen";
+import Guide from "./mainpages/Guide";
 import { Icon } from "react-native-elements";
 import { createStackNavigator } from "react-navigation-stack";
+import NewsStack from "./navigation/NewsStack";
 import db from "./db";
+import AdminHomeStack from "./navigation/AdminHomeStack";
+
+import ManagersStack from "./comps/Managers/ManagersScreen";
+import UserHandlerStack from "./comps/UserHandler/UserHandlerScreen";
+import EmployeeAuthentication from "./mainpages/EmployeeAuthentication";
+
+// import AsyncStorage from "@react-native-community/async-storage";
 
 export default function App(props) {
   const [loggedIn, setLoggedIn] = useState(false);
   const [user, setUser] = useState(null);
+  const [firstLaunch, setFirstLaunch] = useState(false);
+  const [guideView, setGuideView] = useState(true);
+  const [admin, setAdmin] = useState(null);
 
-  const handleLogout = () => {
-    firebase.auth().signOut();
+  const handleLogout = async () => {
+    const userInfo = await db
+      .collection("users")
+      .doc(firebase.auth().currentUser.uid)
+      .get();
+    if (userInfo.data().role === "guest") {
+      await fetch(
+        `https://us-central1-capstone2020-b64fd.cloudfunctions.net/deleteGuestUser?uid=${
+          firebase.auth().currentUser.uid
+        }`
+      );
+      firebase.auth().signOut();
+    } else {
+      firebase.auth().signOut();
+    }
+  };
+
+  const Test = () => {
+    return (
+      <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+        <Text>Test</Text>
+      </View>
+    );
   };
 
   const DashboardTabNavigator = createBottomTabNavigator(
     {
       Home: HomeStack,
+
+      News: NewsStack,
+
       Profile: ProfileStack,
     },
+    // {
+    //   navigationOptions: ({ navigation }) => {
+    //     const { routeName } = navigation.state.routes[navigation.state.index];
+    //     return {
+    //       headerShown: true,
+    //       headerTitle: routeName,
+    //     };
+    //   },
+    // },
     {
-      navigationOptions: ({ navigation }) => {
-        const { routeName } = navigation.state.routes[navigation.state.index];
-        return {
-          headerShown: true,
-          headerTitle: routeName,
-        };
+      tabBarOptions: {
+        activeTintColor: "white",
+        inactiveTintColor: "gray",
+        style: { backgroundColor: "#20365F" },
       },
     }
   );
 
-
-  const FriendsStk = createStackNavigator(
-    { Friends: FriendsStack },
+  const DashboardStackNavigator = createStackNavigator(
+    {
+      DashboardTabNavigator: DashboardTabNavigator,
+    },
     {
       // defaultNavigationOptions: ({ navigation }) => {
       //   return {
@@ -80,14 +123,41 @@ export default function App(props) {
     }
   );
 
+  const FriendsStk = createStackNavigator(
+    { Friends: FriendsStack },
+    {
+      // initialRouteName: "FriendsList",
+
+      defaultNavigationOptions: ({ navigation }) => {
+        return {
+          headerLeft: (
+            <Icon
+              style={{ paddingRight: 10 }}
+              onPress={() => navigation.FriendsList}
+              name="md-menu"
+              type="ionicon"
+              color="white"
+              size={30}
+            />
+          ),
+          headerStyle: {
+            backgroundColor: "#20365F",
+          },
+          headerTitle: "Friends List",
+          headerTintColor: "white",
+        };
+      },
+    }
+  );
+
   const AppDrawerNavigator = createDrawerNavigator(
     {
       Home: {
         screen: DashboardStackNavigator,
       },
-      Friends: {
-        screen: FriendsStk,
-      },
+      // Friends: {
+      //   screen: FriendsStk,
+      // },
     },
     {
       drawerBackgroundColor: "#F0F8FF",
@@ -126,7 +196,7 @@ export default function App(props) {
           </ScrollView>
           <View>
             <TouchableOpacity onPress={handleLogout}>
-              <Text>Logout</Text>
+              <Text>Logout {user && user.displayName}</Text>
             </TouchableOpacity>
           </View>
         </SafeAreaView>
@@ -134,44 +204,37 @@ export default function App(props) {
     }
   );
 
-const TabNavigator = createBottomTabNavigator({
-  Home: HomeStack,
-});
+  const AppContainer = createAppContainer(AppDrawerNavigator);
 
+  async function getUser() {
+    db.collection("users")
+      .doc(firebase.auth().currentUser.uid)
+      .onSnapshot(async (userRef) => {
+        const getAdmin = firebase.functions().httpsCallable("getAdmin");
+        const response = await getAdmin({
+          email: firebase.auth().currentUser.email,
+        });
 
-async function getUser() {
-  db.collection("users")
-    .doc(firebase.auth().currentUser.uid)
-    .onSnapshot(async (userRef) => {
-      const getAdmin = firebase.functions().httpsCallable("getAdmin");
-      const response = await getAdmin({
-        email: firebase.auth().currentUser.email,
+        const admin = response.data.result !== undefined ? true : false;
+
+        const user = { ...userRef.data(), admin };
+        console.log("userROLE", user.role.slice(-12));
+        setUser(user);
       });
+  }
 
-      const admin = response.data.result !== undefined ? true : false;
-
-      const user = { ...userRef.data(), admin };
-      console.log("userROLE", user.role.slice(-12));
-      setUser(user);
-    });
-}
-
-  const getFirstLaunch = async () => {
-    try {
-      const value = await AsyncStorage.getItem("alreadyLaunched");
-      console.log("valueeeeeeeeeeeee", value);
-      if (!value) {
-        await AsyncStorage.setItem("alreadyLaunched", "true");
-        console.log("trueeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee");
-        setFirstLaunch(true);
-      } else {
-        console.log("falseeeeeeeeeeeeeeeeeeeeeee");
-        setFirstLaunch(false);
-      }
-    } catch (err) {
-      console.log(err);
+  async function getFirstLaunch() {
+    const value = await AsyncStorage.getItem("alreadyLaunched");
+    console.log("valueeeeeeeeeeeee", value);
+    if (value === null) {
+      await AsyncStorage.setItem("alreadyLaunched", "true");
+      console.log("trueeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee");
+      setFirstLaunch(true);
+    } else {
+      console.log("falseeeeeeeeeeeeeeeeeeeeeee");
+      setFirstLaunch(false);
     }
-  };
+  }
 
   useEffect(() => {
     getFirstLaunch();
@@ -190,12 +253,13 @@ async function getUser() {
   }, []);
 
   const adminTabNav = createBottomTabNavigator({
+    Home: AdminHomeStack,
     Profile: ProfileStack,
   });
   const AdminAppContainer = createAppContainer(adminTabNav);
 
   const guideSkip = () => {
-    console.log("Skipppped");
+    // console.log("Skipppped");
     setGuideView(false);
   };
 
