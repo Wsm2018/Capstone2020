7cddb83cb536eeb533a7b0e9e9fe98e4605f6bd2
aminehmo <57699481+aminehmo@users.noreply.github.com>
2020-06-05 02:01:18 +0300

    admin and roles design

--- comps/Admin/Home.js
+++ comps/Admin/Home.js
@@ -22,25 +22,56 @@ export default function Home(props) {
   };
   const items = [
     {
-      name: "Promote Users",
-      code: "#1abc9c",
-      nav: () => props.navigation.navigate("MakeAdmin"),
-      image: require("../../assets/images/bookit.png"),
-      height: 50,
-      width: 150,
+      name: "Users",
+      code: "#216380",
+      nav: () => props.navigation.navigate("Users"),
+      image: require("../../assets/images/adminpic/list.jpg"),
+      height: 120,
+      width: 200,
     },
     {
-      name: "EMERALD",
-      code: "#2ecc71",
-      nav: () => props.navigation.navigate("MakeAdmin"),
-      image: require("../../assets/images/chip.png"),
-      height: 100,
-      width: 100,
+      name: "Statistics",
+      code: "#216380",
+      nav: () => props.navigation.navigate("Statistics"),
+      image: require("../../assets/images/adminpic/charts.jpg"),
+      height: 120,
+      width: 200,
     },
-    { name: "PETER RIVER", code: "#3498db" },
-    { name: "AMETHYST", code: "#9b59b6" },
-    { name: "WET ASPHALT", code: "#34495e" },
-    { name: "GREEN SEA", code: "#16a085" },
+
+    {
+      name: "Booking",
+      code: "#216380",
+      nav: () => props.navigation.navigate("Types"),
+      image: require("../../assets/images/adminpic/bookings.jpg"),
+      height: 120,
+      width: 200,
+    },
+    {
+      name: "Make Admin",
+      code: "#216380",
+      nav: () => props.navigation.navigate("MakeAdmin"),
+      image: require("../../assets/images/adminpic/admin.jpg"),
+      height: 120,
+      width: 200,
+    },
+
+    {
+      name: "Promotion",
+      code: "#216380",
+      nav: () => props.navigation.navigate("Promotion"),
+      image: require("../../assets/images/adminpic/promotion.jpg"),
+      height: 120,
+      width: 200,
+    },
+
+    // {
+    //   name: "Change Role",
+    //   code: "#16a085",
+    //   nav: () => handleChangeRole(),
+    //   image: require("../../assets/images/chip.png"),
+    //   height: 100,
+    //   width: 100,
+    // },
     // { name: "NEPHRITIS", code: "#27ae60" },
     // { name: "BELIZE HOLE", code: "#2980b9" },
     // { name: "WISTERIA", code: "#8e44ad" },
@@ -65,300 +96,80 @@ export default function Home(props) {
 
   return (
     // <View style={styles.container}>
-    <FlatGrid
-      itemDimension={150}
-      items={items}
-      style={styles.gridView}
-      // staticDimension={300}
-      // fixed
-      // spacing={20}
-      renderItem={({ item, index }) => (
-        <View style={[styles.itemContainer, { backgroundColor: item.code }]}>
-          <TouchableOpacity onPress={item.nav}>
-            <View style={{ alignItems: "center" }}>
-              <Image
-                // width={Dimensions.get("window").width / 1.3}
-                source={item.image}
-                // autoPlay
-                // loop
-                style={{
-                  height: item.height,
-                  // width: 180,
-                  width: item.width,
-                  // alignItems: "center",
-                  // position: "relative",
-                }}
-              />
-              <Text style={styles.itemName}>{item.name}</Text>
-            </View>
+    <View style={styles.container}>
+      <View>
+        <Text></Text>
+      </View>
+      <FlatGrid
+        itemDimension={150}
+        items={items}
+        style={styles.gridView}
+        renderItem={({ item, index }) => (
+          <View style={[styles.itemContainer, { backgroundColor: item.code }]}>
+            <TouchableOpacity onPress={item.nav}>
+              <View style={{ height: "100%", width: "100%" }}>
+                <Image
+                  source={item.image}
+                  style={{
+                    flex: 1,
+                    height: undefined,
+                    width: undefined,
+                  }}
+                />
 
-            {/* <Text style={styles.itemCode}>{item.code}</Text> */}
-          </TouchableOpacity>
-        </View>
-      )}
-    />
-
-    /* <View
-        width={Dimensions.get("window").width}
+                <View
+                  style={{
+                    alignItems: "center",
+                    flex: 0.2,
+                    justifyContent: "center",
+                  }}
+                >
+                  <Text style={styles.itemName}>{item.name}</Text>
+                </View>
+              </View>
+            </TouchableOpacity>
+          </View>
+        )}
+      />
+      <View
         style={{
-          flex: 0.2,
-          alignItems: "center",
-          justifyContent: "center",
-        }}
-      >
-        <Text>Admin Home</Text>
-      </View> */
-    /* <View
-        width={Dimensions.get("window").width}
-        style={{
-          flex: 0.5,
-          // flexDirection: "row",
-          // flexWrap: "wrap",
-          // backgroundColor: "gray",
-          alignItems: "center",
-          justifyContent: "center",
+          alignItems: "flex-end",
         }}
       >
         <TouchableOpacity
-          width={Dimensions.get("window").width / 2}
           style={{
-            // flex: 1,
             justifyContent: "center",
             alignItems: "center",
-            backgroundColor: "red",
-
-            // justifyContent: "flex-end",
-            borderRadius: 5,
-            padding: 10,
-            height: 100,
+            backgroundColor: "#216380",
+            width: 75,
+            height: 75,
+            borderRadius: 75,
+            borderColor: "#e3e3e3",
+            borderWidth: 6,
           }}
           onPress={() => handleChangeRole()}
         >
-          <Text>Change Role</Text>
+          <Text style={{ color: "#e3e3e3", fontSize: 18, fontWeight: "bold" }}>
+            Roles
+          </Text>
         </TouchableOpacity>
       </View>
-      <View
-        style={{
-          flex: 0.5,
-          flexDirection: "row",
-          justifyContent: "space-around",
-        }}
-      >
-        <View
-          width={Dimensions.get("window").width / 2}
-          style={
-            {
-              // justifyContent: "center",
-              // alignItems: "stretch",
-              // backgroundColor: "blue",
-            }
-          }
-        > */
-    /* <TouchableOpacity
-            // width={Dimensions.get("window").width / 2}
-            style={{
-              // flex: 1,
-              justifyContent: "center",
-              alignItems: "center",
-              backgroundColor: "red",
-
-              // justifyContent: "flex-end",
-              borderRadius: 5,
-              padding: "10%",
-              height: "80%",
-            }}
-            onPress={() => props.navigation.navigate("Users")}
-          >
-            <Text>Users List</Text>
-          </TouchableOpacity>
-        </View>
-
-        <View
-          width={Dimensions.get("window").width / 2}
-          style={
-            {
-              // justifyContent: "center",
-              // alignItems: "stretch",
-              // backgroundColor: "green",
-            }
-          }
-        >
-          <TouchableOpacity
-            // width={Dimensions.get("window").width / 2}
-            style={{
-              // flex: 1,
-              justifyContent: "center",
-              alignItems: "center",
-              backgroundColor: "green",
-
-              // justifyContent: "flex-end",
-              borderRadius: 5,
-              padding: "10%",
-              height: "80%",
-            }}
-            onPress={() => props.navigation.navigate("Statistics")}
-          >
-            <Text>Statistics</Text>
-          </TouchableOpacity>
-        </View>
-      </View>
-
-      <View
-        style={{
-          flex: 0.5,
-          flexDirection: "row",
-          // flexWrap: "wrap",
-          justifyContent: "space-evenly",
-        }}
-      >
-        <View
-          width={Dimensions.get("window").width / 2}
-          style={
-            {
-              // justifyContent: "center",
-              // alignItems: "stretch",
-              // backgroundColor: "blue",
-            }
-          }
-        >
-          <TouchableOpacity
-            // width={Dimensions.get("window").width / 2}
-            style={{
-              // flex: 1,
-              justifyContent: "center",
-              alignItems: "center",
-              backgroundColor: "yellow",
-
-              // justifyContent: "flex-end",
-              borderRadius: 5,
-              padding: "10%",
-              height: "80%",
-            }}
-            onPress={() => props.navigation.navigate("Promotion")}
-          >
-            <Text>Promotion Code</Text>
-          </TouchableOpacity>
-        </View>
-        <View
-          width={Dimensions.get("window").width / 2}
-          style={
-            {
-              // justifyContent: "center",
-              // alignItems: "stretch",
-              // backgroundColor: "blue",
-            }
-          }
-        >
-          <TouchableOpacity
-            // width={Dimensions.get("window").width / 2}
-            style={{
-              // flex: 1,
-              justifyContent: "center",
-              alignItems: "center",
-              backgroundColor: "lightblue",
-
-              // justifyContent: "flex-end",
-              borderRadius: 5,
-              padding: "10%",
-              height: "80%",
-            }}
-            onPress={() => handleLogout()}
-          >
-            <Text>Logout</Text>
-          </TouchableOpacity>
-        </View>
-      </View>
-
-      <View
-        style={{
-          flex: 0.5,
-          flexDirection: "row",
-          // flexWrap: "wrap",
-          justifyContent: "space-evenly",
-        }}
-      >
-        <View
-          width={Dimensions.get("window").width / 2}
-          style={
-            {
-              // justifyContent: "center",
-              // alignItems: "stretch",
-              // backgroundColor: "blue",
-            }
-          }
-        >
-          <TouchableOpacity
-            // width={Dimensions.get("window").width / 2}
-            style={{
-              // flex: 1,
-              justifyContent: "center",
-              alignItems: "center",
-              backgroundColor: "pink",
-
-              // justifyContent: "flex-end",
-              borderRadius: 5,
-              padding: "10%",
-              height: "80%",
-            }}
-            onPress={() => props.navigation.navigate("MakeAdmin")}
-          >
-            <Text>Make Admin</Text>
-          </TouchableOpacity>
-        </View>
-        <View
-          width={Dimensions.get("window").width / 2}
-          style={
-            {
-              // justifyContent: "",
-              // alignItems: "stretch",
-              // backgroundColor: "blue",
-            }
-          }
-        >
-          <TouchableOpacity
-            // width={Dimensions.get("window").width / 2}
-            style={{
-              // flex: 1,
-              justifyContent: "center",
-              alignItems: "center",
-              backgroundColor: "gold",
-
-              // justifyContent: "flex-end",
-              borderRadius: 5,
-              padding: "10%",
-              height: "80%",
-            }}
-            onPress={() => props.navigation.navigate("Types")}
-          >
-            <Text>Booking</Text>
-          </TouchableOpacity>
-        </View>
-      </View> */
-    /* </View> */
+    </View>
   );
 }
 
-// const styles = StyleSheet.create({
-//   container: {
-//     // flex: 1,
-//     // flexDirection: "row",
-//     // flexWrap: "wrap",
-//     // backgroundColor: "#fff",
-//     // alignItems: "center",
-//     // justifyContent: "space-evenly",
-//   },
-// });
-
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#e3e3e3",
+  },
   gridView: {
     marginTop: 20,
     flex: 1,
   },
   itemContainer: {
-    // width: "100%",
     justifyContent: "flex-end",
     borderRadius: 5,
-    padding: 10,
     height: 150,
   },
   itemName: {
